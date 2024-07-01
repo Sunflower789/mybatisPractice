a85c3f2efce3d18ffea48fcf7b8cd3c8731abddb
Sunflower789 <sunflower_hx_9@163.com>
2024-07-01 14:35:27 +0800

    增加mybatisPlus的使用

---
+++ mybatisPlus/src/main/java/com/demo/entity/User.java
@@ -0,0 +1,39 @@
+package com.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author author
+ * @since 2024-05-08
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("tb_user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    private Integer age;
+
+    private String name;
+
+    private String userLevel;
+
+    @TableField(value = "", exist = false, select = false)  //value设置表名，exist为false表没有此字段，select为false不参与查询返回
+    private String remark;
+}
