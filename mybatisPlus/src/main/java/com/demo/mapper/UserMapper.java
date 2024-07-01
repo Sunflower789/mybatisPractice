2fce233b3ca3341c3d1e95d446c9905b1166b983
Sunflower789 <sunflower_hx_9@163.com>
2024-07-01 14:34:41 +0800

    增加mybatisPlus的使用

---
+++ mybatisPlus/src/main/java/com/demo/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.demo.mapper;
+
+import com.demo.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author author
+ * @since 2024-05-08
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ mybatisPlus/src/main/java/com/demo/service/IUserService.java
@@ -0,0 +1,16 @@
+package com.demo.service;
+
+import com.demo.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author author
+ * @since 2024-05-08
+ */
+public interface IUserService extends IService<User> {
+
+}
